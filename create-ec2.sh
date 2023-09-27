5c4b300dd4d06530d6a5747ca1294ad238099297
Kishore <kishore@ficha.fr>
2023-09-27 11:09:51 +0530

    added script for creation of ec2-instances

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,7 +2,7 @@
 NAMES=("mongodb" "mysql" "rabbitmq" "redis" "catalogue" "cart" "user" "payment" "dispatch" "shipping" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
-SECURITY_GROUP=sg-01ea0eafb8976901c
+SECURITY_GROUP_ID=sg-01ea0eafb8976901c
 for i in "${NAMES[@]}"
 do
 if [[ $i == "mongodb" || $i == "mysql" ]];
@@ -11,8 +11,9 @@ then
 else
     INSTANCE_TYPE="t2.micro"
 fi
-    echo "The created instance is $i"
-aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP
+    echo "Creating instance is $i"
+IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress') 
+    echo "The created instance $i : $IP_ADDRESS"
 done
 
 
