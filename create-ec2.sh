613954f647562bd8ec8c812d6dbf8e70de7d5b01
Kishore <kishore@ficha.fr>
2023-09-27 11:25:06 +0530

    added script for creation of ec2-instances

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+NAME=$@
 NAMES=("mongodb" "mysql" "rabbitmq" "redis" "catalogue" "cart" "user" "payment" "dispatch" "shipping" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-01ea0eafb8976901c
-for i in "${NAMES[@]}"
+DOMAIN_NAME=kishoremerugudevops.online
+HOSTED_ZONE_ID=Z00920562NQGNYOPKAAQ3
+for i in $@
 do
 if [[ $i == "mongodb" || $i == "mysql" ]];
 then
@@ -14,6 +17,19 @@ fi
     echo "Creating instance is $i"
 IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress') 
     echo "The created instance $i : $IP_ADDRESS"
-done
 
-
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
+{
+            "Comment": "CREATE/DELETE/UPSERT a record ",
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "$i.$DOMAIN_NAME",
+                                    "Type": "A",
+                                    "TTL": 300,
+                                 "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+}}]
+}
+,
+done 
+ 
